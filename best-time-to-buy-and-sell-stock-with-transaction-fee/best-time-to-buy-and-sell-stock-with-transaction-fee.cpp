076e012d48cf94cc4a1a519d96d06cce119d97f1
Aditya Anerao <39512101+aadeetyeah@users.noreply.github.com>
2021-11-26 11:01:33 +0530

    Time: 291 ms (5.06%), Space: 95.6 MB (5.20%) - LeetHub

---
+++ best-time-to-buy-and-sell-stock-with-transaction-fee/best-time-to-buy-and-sell-stock-with-transaction-fee.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices, int fee) {
+        std::vector<std::vector<int>> v1(prices.size(),std::vector<int>(2,-1));
+        return bestBuyAndSell(0,prices,1,v1,fee);
+    }
+    
+    int bestBuyAndSell(int start,std::vector<int>& prices,int canBuy,std::vector<std::vector<int>>& v1,int fee){
+        if( start>=prices.size()){
+            return 0;
+        }
+        //std::string key=to_string(start)+"_"+to_string(canBuy)+"_"+to_string(TRX_CNT);
+        if(v1[start][canBuy]!=-1){
+            return v1[start][canBuy];
+        }
+        int idle=bestBuyAndSell(start+1,prices,canBuy,v1,fee);
+        if(canBuy){
+            int buy=-prices[start]+bestBuyAndSell(start+1,prices,0,v1,fee);
+            v1[start][canBuy]= std::max(idle,buy);
+            
+        }else{
+            int sell=-fee+prices[start]+bestBuyAndSell(start+1,prices,1,v1,fee);
+            v1[start][canBuy]= std::max(idle,sell);
+        }
+        return v1[start][canBuy];
+    }
+};
