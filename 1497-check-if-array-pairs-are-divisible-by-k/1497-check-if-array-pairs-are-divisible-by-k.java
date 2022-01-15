78fd7649438a7ac06ce7d113fed60671c681d599
Aditya Anerao <39512101+aadeetyeah@users.noreply.github.com>
2022-01-15 13:36:52 +0530

    Time: 93 ms (10.85%), Space: 110.8 MB (5.23%) - LeetHub

--- 1497-check-if-array-pairs-are-divisible-by-k/1497-check-if-array-pairs-are-divisible-by-k.java
+++ 1497-check-if-array-pairs-are-divisible-by-k/1497-check-if-array-pairs-are-divisible-by-k.java
@@ -1,25 +1,24 @@
 class Solution {
     public boolean canArrange(int[] arr, int k) {
-            HashMap<Integer,Integer> h1=new HashMap<>();
-        int temp=0;
+        HashMap<Integer,Integer> map =new HashMap<>();
+        int num=0;
         for(int itr1=0;itr1<arr.length;itr1++){
-            temp=((arr[itr1]%k)+k)%k;
-            h1.put(temp,h1.getOrDefault(temp,0)+1);
+            num=((arr[itr1]%k)+k)%k;            //for handling negative values.
+            map.put(num,map.getOrDefault(num,0)+1);
         }
-        int key;
-        int val;
-        int rem;
-        for(Map.Entry<Integer,Integer> e1 : h1.entrySet()){
-            key=e1.getKey();
-            val=e1.getValue();
-            rem=(k-key)%k;
-            if(key==0 && h1.get(0)%2==1){
+        
+        int rem=0;
+        for(Integer i : map.keySet()){
+             rem=(k-i)%k;
+            if(i==0 && map.get(i)%2==1){
+                System.out.println(1+" "+map.get(i));
                 return false;
-            }else if(h1.get(rem)==null || h1.get(rem)!=val){
+            }else if(map.get(rem)==null || !map.get(i).equals(map.get(rem))){
+                System.out.println(map.get(rem)+"----"+map.get(i));
                 return false;
             }
+            
         }
         return true;
-    
     }
 }
