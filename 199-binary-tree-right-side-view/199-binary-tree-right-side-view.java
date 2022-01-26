47f6ebf12b26b4bd1adbad8c8ef9f4c94b2235b8
Aditya Anerao <39512101+aadeetyeah@users.noreply.github.com>
2022-01-26 16:28:48 +0530

    Time: 4 ms (11.30%), Space: 39.8 MB (7.30%) - LeetHub

--- 199-binary-tree-right-side-view/199-binary-tree-right-side-view.java
+++ 199-binary-tree-right-side-view/199-binary-tree-right-side-view.java
@@ -13,21 +13,34 @@
  *     }
  * }
  */
+
+/* Iterative Solution */
 class Solution {
     public List<Integer> rightSideView(TreeNode root) {
+        if(root==null){
+            return new ArrayList<Integer>();
+        }
+        int ht=0;
         Map<Integer,Integer> m1=new HashMap<>();
-        rightView(root,0,m1);
+        Queue<TreeNode> q1=new LinkedList<>();
+        q1.add(root);
+        while(!q1.isEmpty()){
+            int size=q1.size();
+            while(size>0){
+                TreeNode temp=q1.remove();
+                if(!m1.containsKey(ht)){
+                    m1.put(ht,temp.val);
+                }
+                if(temp.right!=null){
+                    q1.add(temp.right);
+                }
+                if(temp.left!=null){
+                    q1.add(temp.left);
+                }
+                size--;
+            }
+            ht++;
+        }
         return new ArrayList<Integer>(m1.values());
-    }
-    private void rightView(TreeNode root,int ht,Map<Integer,Integer> m1){
-           if(root==null){
-            return;
-        }
-        if(!m1.containsKey(ht)){
-            m1.put(ht,root.val);
-        }
-        rightView(root.right,ht+1,m1);
-        rightView(root.left,ht+1,m1);
-        
     }
 }
