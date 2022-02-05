df2bbe6b08e1fb0112303bdf6fa6e5c3adc90d71
Aditya Anerao <39512101+aadeetyeah@users.noreply.github.com>
2022-02-06 00:59:43 +0530

    Time: 47 ms (5.04%), Space: 71 MB (5.01%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.java
+++ 200-number-of-islands/200-number-of-islands.java
@@ -1,10 +1,9 @@
 class Solution {
     public int numIslands(char[][] grid) {
-        int count=0;
-        boolean flag=false;
+         int count=0;
         for(int itr1=0;itr1<grid.length;itr1++){
             for(int itr2=0;itr2<grid[0].length;itr2++){
-                if(getNumOfIslands(itr1,itr2,grid,flag)){
+                if(getNumOfIslands(itr1,itr2,grid)){
                     count++;
                 }
             }
@@ -12,17 +11,26 @@ class Solution {
         return count;
     }
     
-    private boolean getNumOfIslands(int sr,int sc,char grid[][],boolean flag){
-        if(sr>=grid.length || sc>=grid[0].length || sr<0 || sc<0 || grid[sr][sc]=='0'){
-            return false;
-        }
-        grid[sr][sc]='0';
-        flag=true;
-        getNumOfIslands(sr-1,sc,grid,flag);
-        getNumOfIslands(sr+1,sc,grid,flag);
-        getNumOfIslands(sr,sc-1,grid,flag);
-        getNumOfIslands(sr,sc+1,grid,flag);
+    private boolean getNumOfIslands(int sr,int sc,char[][] grid){
+        Queue<Pair<Integer,Integer>> q1=new LinkedList<>();
+        boolean flag=false;
+        q1.add(new Pair<>(sr,sc));
         
+        while(!q1.isEmpty()){
+            Pair<Integer,Integer> p1=q1.remove();
+            int temp_sr=p1.getKey();
+            int temp_sc=p1.getValue();
+            if(temp_sr>=grid.length || temp_sc>=grid[0].length || temp_sr<0 || temp_sc<0 || grid[temp_sr][temp_sc]=='0'){
+                continue;
+            }
+            
+            grid[temp_sr][temp_sc]='0';
+            flag=true;
+            q1.add(new Pair(temp_sr-1,temp_sc));
+            q1.add(new Pair(temp_sr+1,temp_sc));
+            q1.add(new Pair(temp_sr,temp_sc-1));
+            q1.add(new Pair(temp_sr,temp_sc+1));
+        }
         return flag;
     }
 }
