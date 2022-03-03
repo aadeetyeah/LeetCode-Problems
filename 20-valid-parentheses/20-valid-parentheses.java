2fbb34f08e3636193847207bd8b6389de8dd7aa0
Aditya Anerao <39512101+aadeetyeah@users.noreply.github.com>
2022-03-03 15:12:53 +0530

    Time: 2 ms (79.61%), Space: 41.9 MB (32.50%) - LeetHub

--- 20-valid-parentheses/20-valid-parentheses.java
+++ 20-valid-parentheses/20-valid-parentheses.java
@@ -1,29 +1,32 @@
 class Solution {
     public boolean isValid(String s) {
-        Stack<Character> s1=new Stack<>();
         
+        Stack<Character> s1=new Stack<>();
         
         for(int itr1=0;itr1<s.length();itr1++){
             if(s.charAt(itr1)=='(' || s.charAt(itr1)=='[' || s.charAt(itr1)=='{'){
-                s1.push(s.charAt(itr1));    
+                s1.push(s.charAt(itr1));
             }else{
                 if(s1.isEmpty()){
                     return false;
                 }
-                if(s1.peek()=='(' && s.charAt(itr1)==')'){
+                if(s.charAt(itr1)==')' && s1.peek()=='('){
                     s1.pop();
-                }else if(s1.peek()=='[' && s.charAt(itr1)==']'){
+                }else if(s.charAt(itr1)=='}' && s1.peek()=='{'){
                     s1.pop();
-                }else if(s1.peek()=='{' && s.charAt(itr1)=='}'){
+                }else if(s.charAt(itr1)==']' && s1.peek()=='['){
                     s1.pop();
                 }else{
                     return false;
                 }
             }
         }
-        if(s1.isEmpty()){
-            return true;
+        if(!s1.isEmpty()){
+            return false;
         }
-        return false;
+        return true;
+        }
+        
+        
     }
-}
+    
