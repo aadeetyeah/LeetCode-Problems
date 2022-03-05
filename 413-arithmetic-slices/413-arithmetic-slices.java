318a1c4dbc283e0b7295b535b9ad626be0e50b0f
Aditya Anerao <39512101+aadeetyeah@users.noreply.github.com>
2022-03-06 01:36:22 +0530

    Time: 0 ms (100.00%), Space: 40.2 MB (78.29%) - LeetHub

--- 413-arithmetic-slices/413-arithmetic-slices.java
+++ 413-arithmetic-slices/413-arithmetic-slices.java
@@ -1,5 +1,5 @@
 class Solution {
-     public int numberOfArithmeticSlices(int[] nums) {
+     /*public int numberOfArithmeticSlices(int[] nums) {
         if(nums==null || nums.length==0)
         {
             return 0;
@@ -15,5 +15,25 @@ class Solution {
             }
         }
         return res;
+    } */
+    
+    public int numberOfArithmeticSlices(int[] nums) {
+        if(nums.length<3 || nums==null){
+            return 0;
+        }
+        int arr[]=new int[nums.length];
+        for(int itr1=2;itr1<nums.length;itr1++){
+            
+            if(nums[itr1]-nums[itr1-1]==nums[itr1-1]-nums[itr1-2]){
+                arr[itr1]=arr[itr1-1]+1;
+            }
+        }
+        int count=0;
+        for(int itr1=2;itr1<arr.length;itr1++){
+            count+=arr[itr1];
+        }
+        return count;
+        
     }
+
 }
