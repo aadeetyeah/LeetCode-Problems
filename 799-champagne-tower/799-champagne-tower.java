b5e66ba7b6713e5fbe640cfed4d562b1d0cd30a8
Aditya Anerao <39512101+aadeetyeah@users.noreply.github.com>
2022-03-13 00:59:59 +0530

    Time: 43 ms (5.02%), Space: 55.8 MB (16.07%) - LeetHub

---
+++ 799-champagne-tower/799-champagne-tower.java
@@ -0,0 +1,21 @@
+class Solution {
+    public double champagneTower(int poured1, int query_row, int query_glass) {
+        double dp[][]=new double[101][101];
+    
+        dp[0][0]=(double)poured1;
+        double poured=0;
+        for(int itr1=0;itr1<query_row+1;itr1++){
+            System.out.println(itr1);
+            for(int itr2=0;itr2<=itr1;itr2++){
+                if(dp[itr1][itr2]>1){
+                    poured=dp[itr1][itr2];
+                    dp[itr1][itr2]=1;
+                    poured=poured-dp[itr1][itr2];
+                    dp[itr1+1][itr2]+=poured/2;
+                    dp[itr1+1][itr2+1]+=poured/2;
+                }
+            }
+        }
+        return dp[query_row][query_glass];
+    }
+}
