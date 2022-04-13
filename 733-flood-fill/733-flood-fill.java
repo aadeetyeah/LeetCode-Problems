32872cb1554dcf4f38564c36bad5143ba33970e7
Aditya Anerao <39512101+aadeetyeah@users.noreply.github.com>
2022-04-14 00:37:16 +0530

    Time: 2 ms (30.33%), Space: 43.3 MB (83.77%) - LeetHub

--- 733-flood-fill/733-flood-fill.java
+++ 733-flood-fill/733-flood-fill.java
@@ -1,23 +1,36 @@
 class Solution {
     public int[][] floodFill(int[][] image, int sr, int sc, int newColor) {
+        
         if(image[sr][sc]==newColor){
             return image;
         }
-        fillNewColor(sr,sc,image[sr][sc],newColor,image);
+        iterativeFloodFill(image,sr,sc,newColor);
         return image;
     }
-    
-    private void fillNewColor(int sr,int sc,int currentColor,int newColor,int[][] image){
+    private void iterativeFloodFill(int[][] image,int sr,int sc,int newColor){
+        int currentColor=image[sr][sc];
         
-        if(sr<0 || sr>=image.length || sc<0 || sc>=image[0].length || image[sr][sc]!=currentColor){
-            return;
+        int rows=image.length;
+        int cols=image[0].length;
+        Queue<Pair<Integer,Integer>> q1=new LinkedList<>();
+        Pair<Integer,Integer> p1=new Pair<>(sr,sc);
+        q1.add(p1);
+        while(!q1.isEmpty()){
+            Pair<Integer,Integer> tempP=q1.remove();
+            int srTemp=tempP.getKey();
+            int scTemp=tempP.getValue();
+            
+            if(srTemp>=rows || srTemp<0 || scTemp<0 || scTemp>=cols || image[srTemp][scTemp]!=currentColor){
+                continue;
+            }
+            
+            image[srTemp][scTemp]=newColor;
+            q1.add(new Pair<Integer,Integer>(srTemp+1,scTemp));
+            q1.add(new Pair<Integer,Integer>(srTemp-1,scTemp));
+            q1.add(new Pair<Integer,Integer>(srTemp,scTemp-1));
+            q1.add(new Pair<Integer,Integer>(srTemp,scTemp+1));
+            
         }
-        image[sr][sc]=newColor;
-        
-        fillNewColor(sr+1,sc,currentColor,newColor,image);
-        fillNewColor(sr-1,sc,currentColor,newColor,image);
-        fillNewColor(sr,sc+1,currentColor,newColor,image);
-        fillNewColor(sr,sc-1,currentColor,newColor,image);
         return;
     }
 }
