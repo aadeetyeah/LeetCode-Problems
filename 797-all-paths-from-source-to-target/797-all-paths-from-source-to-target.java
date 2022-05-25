edbf917c5bd391f1a09c35654ec6164102238bae
Aditya Anerao <39512101+aadeetyeah@users.noreply.github.com>
2022-05-26 00:38:13 +0530

    Time: 17 ms (6.79%), Space: 55 MB (49.17%) - LeetHub

--- 797-all-paths-from-source-to-target/797-all-paths-from-source-to-target.java
+++ 797-all-paths-from-source-to-target/797-all-paths-from-source-to-target.java
@@ -1,24 +1,28 @@
 class Solution {
     public List<List<Integer>> allPathsSourceTarget(int[][] graph) {
+        Queue<List<Integer>> q1=new LinkedList<>();
+        
+        List<Integer> l1=new ArrayList<>();
+        l1.add(0);
+        
+        q1.add(l1);
+        
         List<List<Integer>> answer=new ArrayList<>();
         
-        getAllPaths(0,graph.length-1,graph,answer,new ArrayList<Integer>());
+        while(!q1.isEmpty()){
+            
+            List<Integer> t1=q1.remove();
+            int u=t1.get(t1.size()-1);
+            if(u==graph.length-1){
+                answer.add(t1);
+                continue;
+            }
+            for(int i : graph[u]){
+                List<Integer> temp=new ArrayList<>(t1);
+                temp.add(i);
+                q1.add(temp);
+            }
+        }
         return answer;
-    }
-    
-    private void getAllPaths(int src,int target,int graph[][],List<List<Integer>> ans,ArrayList<Integer> res){
-        res.add(src);
-        if(src==target){
-            ans.add(new ArrayList(res));
-            res.remove(res.size()-1);
-            return;
-        }
-        
-        
-        for(int itr1 : graph[src]){
-            getAllPaths(itr1,target,graph,ans,res);
-        }
-        res.remove(res.size()-1);
-        return;
     }
 }
