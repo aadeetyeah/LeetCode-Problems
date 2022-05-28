c83dd72071da2287c570b323574bb04cb27b0bf8
Aditya Anerao <39512101+aadeetyeah@users.noreply.github.com>
2022-05-28 23:57:59 +0530

    Time: 0 ms (100.00%), Space: 43.3 MB (78.78%) - LeetHub

--- 733-flood-fill/733-flood-fill.java
+++ 733-flood-fill/733-flood-fill.java
@@ -4,20 +4,19 @@ class Solution {
         if(image[sr][sc]==newColor){
             return image;
         }
-        fillNewColor(sr,sc,image[sr][sc],newColor,image);
+        changeColor(image,sr,sc,image[sr][sc],newColor);
         return image;
     }
     
-    private void fillNewColor(int sr,int sc,int currColor,int newColor,int[][] image){
-        if(sr<0 || sc<0 || sr>=image.length || sc>=image[0].length || image[sr][sc]!=currColor ){
+    private void changeColor(int image[][],int sr,int sc,int currColor,int newColor){
+        if(sr<0 || sc<0 || sr>=image.length || sc>=image[0].length  || image[sr][sc]!=currColor ){
             return;
         }
         image[sr][sc]=newColor;
-        fillNewColor(sr+1,sc,currColor,newColor,image);
-        fillNewColor(sr-1,sc,currColor,newColor,image);
-        fillNewColor(sr,sc+1,currColor,newColor,image);
-        fillNewColor(sr,sc-1,currColor,newColor,image);
+        changeColor(image,sr+1,sc,currColor,newColor);
+        changeColor(image,sr-1,sc,currColor,newColor);
+        changeColor(image,sr,sc+1,currColor,newColor);
+        changeColor(image,sr,sc-1,currColor,newColor);
         return;
-        
     }
 }
