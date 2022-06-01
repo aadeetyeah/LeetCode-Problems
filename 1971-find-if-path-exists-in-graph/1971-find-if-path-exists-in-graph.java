21cbcb2b3381d9dcbd415b89022dfa66a13c0b48
Aditya Anerao <39512101+aadeetyeah@users.noreply.github.com>
2022-06-01 23:29:57 +0530

    Time: 212 ms (40.54%), Space: 237.4 MB (17.05%) - LeetHub

--- 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.java
+++ 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.java
@@ -1,63 +1,37 @@
 class Solution {
     public boolean validPath(int n, int[][] edges, int source, int destination) {
-        HashMap<Integer,List<Integer>> graph=new HashMap<>();
-        for(int itr1=0;itr1<n;itr1++){
-            graph.put(itr1,new ArrayList<Integer>());
-        }
-        createGraph(graph,edges);
+        List<List<Integer>> graph=createGraph(edges,n);
+        
         int visited[]=new int[n];
-        return checkPath(source,destination,visited,graph);
+        return findValidPath(graph,n,source,destination,visited);
     }
     
-    //Recursive Approach
-    private boolean checkPath(int src,int dest,int visited[],Map<Integer,List<Integer>> graph){
-        visited[src]=1;
+    private boolean findValidPath(List<List<Integer>> graph,int n,int src,int dest,int visited[]){
+        
         if(src==dest){
             return true;
         }
-        
+        visited[src]=1;
         
         for(Integer itr1 : graph.get(src)){
-            if(visited[itr1]==0){
-                if(checkPath(itr1,dest,visited,graph)){
-                    return true;
-                }
+            if(visited[itr1]==0 && findValidPath(graph,n,itr1,dest,visited)){
+                return true;
             }
         }
-       
         return false;
     }
     
-    
-    //Iterative Approach
-    /*private boolean checkPath(int source,int destination,Map<Integer,List<Integer>> graph){
-        Queue<Integer> q1=new LinkedList<>();
-        q1.add(source);
-        int visited[]=new int[graph.size()];
-        
-        while(!q1.isEmpty()){
-            int currentNode=q1.remove();
-            
-            if(currentNode==destination){
-                return true;
-            }
-            visited[currentNode]=1;
-            for(Integer itr1 : graph.get(currentNode)){
-                if(visited[itr1]==0){
-                    q1.add(itr1);
-                }
-            }
-            
+    private List<List<Integer>> createGraph(int edges[][],int n){
+        List<List<Integer>> graph=new ArrayList<>();
+        for(int itr1=0;itr1<n;itr1++){
+            graph.add(new ArrayList<Integer>());
         }
-        return false;
-    }*/
-    
-    private void createGraph(Map<Integer,List<Integer>> graph,int[][] edges){
         for(int itr1=0;itr1<edges.length;itr1++){
             int u=edges[itr1][0];
             int v=edges[itr1][1];
             graph.get(u).add(v);
             graph.get(v).add(u);
         }
+        return graph;
     }
 }
