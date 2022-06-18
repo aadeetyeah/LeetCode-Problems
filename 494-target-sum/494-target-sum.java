7d46d7affafc8d22327905a0194ddb7986bb4f7f
Aditya Anerao <39512101+aadeetyeah@users.noreply.github.com>
2022-06-18 09:28:42 +0530

    Time: 366 ms (39.76%), Space: 118.6 MB (5.17%) - LeetHub

--- 494-target-sum/494-target-sum.java
+++ 494-target-sum/494-target-sum.java
@@ -1,9 +1,15 @@
 class Solution {
     public int findTargetSumWays(int[] nums, int target) {
-        return numOfWays(0,0,nums,target);
+        
+        Map<String,Integer> map1=new HashMap<>();
+        return numOfWays(0,0,nums,target,map1);
     }
     
-    private int numOfWays(int start,int sum,int nums[],int target){
+    private int numOfWays(int start,int sum,int nums[],int target,Map<String,Integer> map1){
+        String key=Integer.toString(start)+"_"+Integer.toString(sum);
+        if(map1.containsKey(key)){
+            return map1.get(key);
+        }
         if(sum==target && start>=nums.length){
             return 1;
         }
@@ -11,6 +17,7 @@ class Solution {
             return 0;
         }
         
-        return numOfWays(start+1,sum+nums[start],nums,target)+numOfWays(start+1,sum-nums[start],nums,target);
+        map1.put(key,numOfWays(start+1,sum+nums[start],nums,target,map1)+numOfWays(start+1,sum-nums[start],nums,target,map1));
+        return map1.get(key);
     }
 }
