efe2c6b8ca7c4f7f1df22f93a010f301152214da
Aditya Anerao <39512101+aadeetyeah@users.noreply.github.com>
2024-01-07 15:48:57 +0530

    Time: 0 ms (100.00%), Space: 44.3 MB (27.16%) - LeetHub

--- 0328-odd-even-linked-list/0328-odd-even-linked-list.java
+++ 0328-odd-even-linked-list/0328-odd-even-linked-list.java
@@ -9,46 +9,82 @@
  * }
  */
 class Solution {
+    
+    
     public ListNode oddEvenList(ListNode head) {
         
-        ListNode oddList = null;
-        ListNode evenList = null;
-        
-        ListNode temp = head;
-        
-        ListNode oddHead = null;
-        ListNode evenHead = null;
         
         if(head == null){
             return null;
         }
+        ListNode temp = head;
+        ListNode evenHead = null;
+        ListNode evenList = null;
         
-        int ind = 0;
-        while(temp!=null){
-            if(ind%2==0){
-                if(oddHead == null){
-                    oddHead = new ListNode(temp.val);
-                    oddList = oddHead;
-                }else{
-                    oddList.next = new ListNode(temp.val);
-                    oddList = oddList.next;
-                }
+        ListNode prevTemp = null;
+        while(temp != null){
+            if(evenHead == null){
+                evenHead = temp.next;
+                evenList = evenHead;
             }else{
-                if(evenHead == null){
-                    evenHead = new ListNode(temp.val);
-                    evenList = evenHead;
-                }else{
-                    evenList.next = new ListNode(temp.val);
-                    evenList = evenList.next;
-                }
+                evenList.next = temp.next;
+                evenList = evenList.next;
             }
             
-            ind++;
+            if(temp.next!=null){
+                temp.next = evenList.next;    
+            }
+            
+            prevTemp = temp;
             temp = temp.next;
         }
         
-        oddList.next = evenHead;
+        prevTemp.next = evenHead;
         
-        return oddHead;
+        return head;
     }
+    
+    //NAIVE APPROACH
+//     public ListNode oddEvenList(ListNode head) {
+        
+//         ListNode oddList = null;
+//         ListNode evenList = null;
+        
+//         ListNode temp = head;
+        
+//         ListNode oddHead = null;
+//         ListNode evenHead = null;
+        
+//         if(head == null){
+//             return null;
+//         }
+        
+//         int ind = 0;
+//         while(temp!=null){
+//             if(ind%2==0){
+//                 if(oddHead == null){
+//                     oddHead = new ListNode(temp.val);
+//                     oddList = oddHead;
+//                 }else{
+//                     oddList.next = new ListNode(temp.val);
+//                     oddList = oddList.next;
+//                 }
+//             }else{
+//                 if(evenHead == null){
+//                     evenHead = new ListNode(temp.val);
+//                     evenList = evenHead;
+//                 }else{
+//                     evenList.next = new ListNode(temp.val);
+//                     evenList = evenList.next;
+//                 }
+//             }
+            
+//             ind++;
+//             temp = temp.next;
+//         }
+        
+//         oddList.next = evenHead;
+        
+//         return oddHead;
+//     }
 }
