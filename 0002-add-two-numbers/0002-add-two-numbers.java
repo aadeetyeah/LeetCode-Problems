b9ab148d9e2848a9d72d70b3ff1538d3deb2183c
Aditya Anerao <39512101+aadeetyeah@users.noreply.github.com>
2024-03-24 12:47:15 +0530

    Time: 1 ms (100.00%), Space: 44.2 MB (80.55%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.java
+++ 0002-add-two-numbers/0002-add-two-numbers.java
@@ -10,6 +10,57 @@
  */
 class Solution {
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        int carry = 0;
+        // return addTwoNumbersIterative(l1,l2);
+        return addTwoNumbersRecursive(l1,l2,carry);
+    }
+    
+    public ListNode addTwoNumbersRecursive(ListNode l1, ListNode l2,int carry) {
+        int num = 0;       
+        
+        if(l1 == null && l2!=null){
+            num = l2.val + carry;    
+        }
+        if(l2 == null && l1!=null){
+            num = l1.val + carry;
+        }
+        if(l1 != null && l2 != null){
+            num = l1.val + l2.val + carry;
+        }
+        
+        if(l1 == null && l2==null){
+            if(carry!=0){
+                return new ListNode(carry);
+            }else{
+                return null;
+            }
+        }
+            
+        carry = 0;
+        if(num>9){
+            carry = num/10;
+            num = num%10;
+        }
+        
+        ListNode newNode = new ListNode(num);
+        
+        if(l1!=null && l2 == null){
+            newNode.next = addTwoNumbersRecursive(l1.next,null,carry);
+        }
+        
+        if(l2!=null && l1==null){
+            newNode.next = addTwoNumbersRecursive(null,l2.next,carry);
+        }
+        
+        if(l2!=null && l1!=null){
+            newNode.next = addTwoNumbersRecursive(l1.next,l2.next,carry);
+        }
+            
+        return newNode;
+    }
+        
+    
+    public ListNode addTwoNumbersIterative(ListNode l1, ListNode l2) {
         
         ListNode temp1 = l1;
         ListNode temp2 = l2;
