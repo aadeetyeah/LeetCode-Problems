167537169855eed5f57ef4b98c5bc46983e37ce2
Aditya Anerao <39512101+aadeetyeah@users.noreply.github.com>
2024-03-25 23:00:13 +0530

    Time: 1 ms (95.57%), Space: 44.2 MB (87.58%) - LeetHub

--- 0046-permutations/0046-permutations.java
+++ 0046-permutations/0046-permutations.java
@@ -2,9 +2,33 @@ class Solution {
     public List<List<Integer>> permute(int[] nums) {
         List<List<Integer>> ans = new ArrayList<>();
         Map<Integer,Integer> cache = new HashMap<>();
-        permutate(0,nums,ans,new ArrayList<Integer>(),cache);
-        
+        //permutate(0,nums,ans,new ArrayList<Integer>(),cache);
+        permutate(0,nums,ans);
         return ans;
+    }
+    
+    
+    private void permutate(int index,int nums[],List<List<Integer>> ans){
+        
+        if(index==nums.length){
+            List<Integer> temp = new ArrayList<>();
+            for(int no : nums){
+                temp.add(no);
+            }
+            ans.add(new ArrayList<Integer>(temp));
+            return;
+        }
+        for(int itr1=index;itr1<nums.length;itr1++){
+            swap(nums,index,itr1);
+            permutate(index+1,nums,ans);
+            swap(nums,index,itr1);
+        }
+    }
+    
+    private void swap(int nums[],int first,int second){
+        int temp = nums[first];
+        nums[first] = nums[second];
+        nums[second] = temp;
     }
     
     private void permutate(int index, int nums[],List<List<Integer>> ans,List<Integer> temp,Map<Integer,Integer> cache){
@@ -24,4 +48,5 @@ class Solution {
             cache.remove(nums[itr1]);
         }
     }
+    
 }
