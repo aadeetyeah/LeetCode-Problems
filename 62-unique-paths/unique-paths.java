8c7ede137deb0c6f0e822f3c8bc3f7efe929e16a
Aditya A <39512101+aadeetyeah@users.noreply.github.com>
2025-10-03 23:53:10 +0530

    Time: 1 ms (6.79%) | Memory: 40.5 MB (48.31%) - LeetSync

--- 62-unique-paths/unique-paths.java
+++ 62-unique-paths/unique-paths.java
@@ -1,7 +1,33 @@
 class Solution {
     public int uniquePaths(int m, int n) {
         Map<String,Integer> memo = new HashMap<>();
-        return uniqueWays(m-1,n-1,memo);
+        // return uniqueWays(m-1,n-1,memo);
+
+        return uniquePathsIterative(m,n);
+    }
+
+    private int uniquePathsIterative(int rows,int columns){
+
+        int dp[][]= new int[rows][columns];
+        dp[0][0]=1;
+        for(int itr1 = 0;itr1<rows;itr1++){
+            for(int itr2=0; itr2<columns; itr2++){
+                int down = 0;
+                int right = 0;
+                if(itr1==0 && itr2==0){
+                    dp[0][0] = 1;
+                }else{
+                    if(itr1>0){
+                        down = dp[itr1-1][itr2];
+                    }
+                    if(itr2>0){
+                        right = dp[itr1][itr2-1];
+                    }
+                    dp[itr1][itr2] = down + right;
+                }
+            }
+        }
+        return dp[rows-1][columns-1];
     }
 
     private int uniqueWays(int itr1, int itr2, Map<String,Integer> memo){
