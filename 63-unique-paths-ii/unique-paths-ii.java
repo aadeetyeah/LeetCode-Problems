c754e46c049ef47cc951f5ba73b52d9783350a84
Aditya A <39512101+aadeetyeah@users.noreply.github.com>
2025-10-10 13:08:50 +0530

    Time: 0 ms (100.00%) | Memory: 42 MB (28.67%) - LeetSync

--- 63-unique-paths-ii/unique-paths-ii.java
+++ 63-unique-paths-ii/unique-paths-ii.java
@@ -1,9 +1,39 @@
 class Solution {
     public int uniquePathsWithObstacles(int[][] obstacleGrid) {
-        int row = obstacleGrid.length-1;
-        int col = obstacleGrid[0].length-1;
-        Map<String,Integer> memo = new HashMap<>();
-        return uniquePath2(row,col,obstacleGrid,memo);
+        // int row = obstacleGrid.length-1;
+        // int col = obstacleGrid[0].length-1;
+        // Map<String,Integer> memo = new HashMap<>();
+        // return uniquePath2(row,col,obstacleGrid,memo);
+        return uniquePathIterative(obstacleGrid);
+    }
+
+    private int uniquePathIterative(int[][] obstacleGrid){
+        int row = obstacleGrid.length;
+        int col = obstacleGrid[0].length;
+        int dp[][] = new int[row][col];
+
+        dp[0][0] = 1;
+        for(int itr1=0; itr1< row;itr1++){
+            for(int itr2=0; itr2<col; itr2++){
+                if(itr1==0 && itr2==0 && obstacleGrid[itr1][itr2]==0){
+                    dp[itr1][itr2] = 1;
+                }else if(obstacleGrid[itr1][itr2]==1){
+                    dp[itr1][itr2] = 0;
+                }else{
+                int up = 0;
+                int left = 0;
+                if(itr1>0){
+                    up = dp[itr1-1][itr2];
+                }
+                if(itr2>0){
+                    left = dp[itr1][itr2-1];
+                }
+                dp[itr1][itr2] = up+left;
+                }
+            }
+
+        }
+        return dp[row-1][col-1];
     }
 
     private int uniquePath2(int row, int col,int[][] obstacleGrid,Map<String,Integer> memo){
