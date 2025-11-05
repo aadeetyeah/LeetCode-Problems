44380baa65718f641ccf39ee0d4fa466ba57ba55
Aditya A <39512101+aadeetyeah@users.noreply.github.com>
2025-11-05 22:27:02 +0530

    Time: 1 ms (6.82%), Space: 42 MB (5.23%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.java
+++ 0062-unique-paths/0062-unique-paths.java
@@ -1,7 +1,8 @@
 class Solution {
     public int uniquePaths(int m, int n) {
-        Map<String,Integer> memo = new HashMap<>();
-        return uniqueWays(m-1,n-1,memo);
+        // Map<String,Integer> memo = new HashMap<>();
+        // return uniqueWays(m-1,n-1,memo);
+        return uniqueWaysIterative(m,n);
     }
 
     private int uniqueWays(int m,int n,Map<String,Integer> memo){
@@ -19,5 +20,27 @@ class Solution {
         int down = uniqueWays(m,n-1,memo);
         memo.put(key,up+down);
         return memo.get(key);
+    }
+
+    private int uniqueWaysIterative(int rows, int cols){
+        int dp[][] = new int[rows][cols];
+        dp[0][0] = 1;
+        for(int itr1=0;itr1<rows;itr1++){
+            for(int itr2=0;itr2<cols;itr2++){
+                if(itr1 == 0 && itr2 == 0){
+                    dp[0][0] = 1;
+                }else{
+                int up = 0;
+                int left = 0;
+                if(itr1>0)
+                    up = dp[itr1-1][itr2];
+                if(itr2>0)
+                    left = dp[itr1][itr2-1];
+                
+                dp[itr1][itr2] = up+left;
+                }
+            }
+        }
+        return dp[rows-1][cols-1];
     }
 }
