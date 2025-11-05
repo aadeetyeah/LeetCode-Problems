6d029035398ca713025208dd2c881b7e1822a8a3
Aditya A <39512101+aadeetyeah@users.noreply.github.com>
2025-11-05 21:10:41 +0530

    Time: 7 ms (1.54%) | Memory: 44.4 MB (5.23%) - LeetSync

--- 62-unique-paths/unique-paths.java
+++ 62-unique-paths/unique-paths.java
@@ -1,51 +1,23 @@
 class Solution {
     public int uniquePaths(int m, int n) {
         Map<String,Integer> memo = new HashMap<>();
-        // return uniqueWays(m-1,n-1,memo);
-
-        return uniquePathsIterative(m,n);
+        return uniqueWays(m-1,n-1,memo);
     }
 
-    private int uniquePathsIterative(int rows,int columns){
-
-        int dp[][]= new int[rows][columns];
-        dp[0][0]=1;
-        for(int itr1 = 0;itr1<rows;itr1++){
-            for(int itr2=0; itr2<columns; itr2++){
-                int down = 0;
-                int right = 0;
-                if(itr1==0 && itr2==0){
-                    dp[0][0] = 1;
-                }else{
-                    if(itr1>0){
-                        down = dp[itr1-1][itr2];
-                    }
-                    if(itr2>0){
-                        right = dp[itr1][itr2-1];
-                    }
-                    dp[itr1][itr2] = down + right;
-                }
-            }
-        }
-        return dp[rows-1][columns-1];
-    }
-
-    private int uniqueWays(int itr1, int itr2, Map<String,Integer> memo){
-        if(itr1<0 || itr2<0){
+    private int uniqueWays(int m,int n,Map<String,Integer> memo){
+        if(m < 0 || n < 0){
             return 0;
         }
-
-        if(itr1==0 && itr2==0){
+        if(m == 0 && n == 0){
             return 1;
         }
-
-        String key = Integer.toString(itr1)+"_"+Integer.toString(itr2);
+        String key = Integer.toString(m)+"_"+Integer.toString(n);
         if(memo.containsKey(key)){
             return memo.get(key);
         }
-        int up = uniqueWays(itr1-1,itr2,memo);
-        int left = uniqueWays(itr1,itr2-1,memo);
-        memo.put(key, up+left);
+        int up = uniqueWays(m-1,n,memo);
+        int down = uniqueWays(m,n-1,memo);
+        memo.put(key,up+down);
         return memo.get(key);
-    }         
+    }
 }
