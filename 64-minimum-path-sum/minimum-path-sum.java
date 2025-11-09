b88e01d37923468021649e99e47a929064852d6d
Aditya A <39512101+aadeetyeah@users.noreply.github.com>
2025-11-09 23:55:29 +0530

    Time: 6 ms (9.68%) | Memory: 50.2 MB (7.21%) - LeetSync

--- 64-minimum-path-sum/minimum-path-sum.java
+++ 64-minimum-path-sum/minimum-path-sum.java
@@ -1,9 +1,35 @@
 class Solution {
     public int minPathSum(int[][] grid) {
+        // int row = grid.length;
+        // int col = grid[0].length;
+        // Map<String,Integer> map = new HashMap<>();
+        // return minPathSum(grid,row-1,col-1,map);
+        return minPathSumIterative(grid);
+    }
+
+    private int minPathSumIterative(int[][] grid){
         int row = grid.length;
         int col = grid[0].length;
-        Map<String,Integer> map = new HashMap<>();
-        return minPathSum(grid,row-1,col-1,map);
+        int dp[][] = new int[row][col];
+
+        for(int itr1 = 0; itr1<row; itr1++){
+            for(int itr2 = 0; itr2<col; itr2++){
+                if(itr1 == 0 && itr2 == 0){
+                    dp[0][0] = grid[0][0];
+                }else{
+                    int up = (int) 1e9;
+                    int left = (int) 1e9;
+                    if(itr1>0)
+                        up = grid[itr1][itr2] + dp[itr1-1][itr2];
+                    if(itr2>0)
+                        left = grid[itr1][itr2] + dp[itr1][itr2-1];
+                    
+                    dp[itr1][itr2] = Math.min(up,left);
+
+                }
+            }
+        } 
+        return dp[row-1][col-1];
     }
 
     private int minPathSum(int[][] grid,int row, int col,Map<String,Integer> map){
