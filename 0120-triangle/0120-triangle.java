0c46bfc56c33b46a71e5cd75ce128475a899854b
Aditya A <39512101+aadeetyeah@users.noreply.github.com>
2025-11-10 10:00:37 +0530

    Time: 13 ms (6.15%), Space: 47.9 MB (5.31%) - LeetHub

--- 0120-triangle/0120-triangle.java
+++ 0120-triangle/0120-triangle.java
@@ -1,25 +1,20 @@
 class Solution {
     public int minimumTotal(List<List<Integer>> triangle) {
-        
         Map<String,Integer> memo = new HashMap<>();
-        return minTotal(triangle,0,0,memo);
+        return minTotal(0,0,triangle,memo);
     }
-    
-    private int minTotal(List<List<Integer>> triangle,int row,int col,Map<String,Integer> memo){
-        
-        if(row==triangle.size()-1){
+
+    private int minTotal(int row, int col, List<List<Integer>> triangle, Map<String,Integer> memo){
+        if(row == triangle.size()-1){
             return triangle.get(row).get(col);
         }
         String key = Integer.toString(row)+"_"+Integer.toString(col);
         if(memo.containsKey(key)){
             return memo.get(key);
         }
-        
-        int down = minTotal(triangle,row+1,col,memo);
-        int diag = minTotal(triangle,row+1,col+1,memo);
-        
-        int result = triangle.get(row).get(col) + Math.min(down,diag);
-        memo.put(key,result);
-        return result;
+        int down = triangle.get(row).get(col) + minTotal(row+1, col, triangle, memo);
+        int right = triangle.get(row).get(col) + minTotal(row+1, col+1,triangle, memo);
+        memo.put(key,Math.min(down,right));
+        return memo.get(key);
     }
 }
