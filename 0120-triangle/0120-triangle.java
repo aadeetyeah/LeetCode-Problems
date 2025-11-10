92ea90a037b67c598c3fc1fe7c6d35d6db2297e8
Aditya A <39512101+aadeetyeah@users.noreply.github.com>
2025-11-10 13:12:21 +0530

    Time: 5 ms (50.93%), Space: 45.5 MB (56.93%) - LeetHub

--- 0120-triangle/0120-triangle.java
+++ 0120-triangle/0120-triangle.java
@@ -1,7 +1,30 @@
 class Solution {
     public int minimumTotal(List<List<Integer>> triangle) {
-        Map<String,Integer> memo = new HashMap<>();
-        return minTotal(0,0,triangle,memo);
+        // Map<String,Integer> memo = new HashMap<>();
+        // return minTotal(0,0,triangle,memo);
+        return minTotalIterative(triangle);
+    }
+
+    private int minTotalIterative(List<List<Integer>> triangle){
+        int dp[][] = new int[triangle.size()][triangle.size()];
+        int lastRowInd = triangle.size()-1;
+        int lastRowLen = triangle.get(lastRowInd).size();
+        //Base Condition.
+        for(int itr1 = 0;itr1<lastRowLen; itr1++){
+            dp[lastRowInd][itr1] = triangle.get(lastRowInd).get(itr1); 
+        }
+
+        int len = triangle.size();
+        for(int itr1 = len-2;itr1>=0;itr1--){
+            for(int itr2=itr1; itr2>=0; itr2--){
+                int down = triangle.get(itr1).get(itr2) + 
+                            dp[itr1+1][itr2];
+                int right = triangle.get(itr1).get(itr2) + 
+                            dp[itr1+1][itr2+1];
+                dp[itr1][itr2] = Math.min(down,right);
+            }
+        }
+        return dp[0][0];
     }
 
     private int minTotal(int row, int col, List<List<Integer>> triangle, Map<String,Integer> memo){
