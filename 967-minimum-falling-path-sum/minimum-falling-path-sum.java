916207fb738d12cbee3d398177e2256efe8a435a
Aditya A <39512101+aadeetyeah@users.noreply.github.com>
2025-11-14 23:46:46 +0530

    Time: 9 ms (17.96%) | Memory: 47.4 MB (7.97%) - LeetSync

--- 967-minimum-falling-path-sum/minimum-falling-path-sum.java
+++ 967-minimum-falling-path-sum/minimum-falling-path-sum.java
@@ -1,6 +1,18 @@
 class Solution {
     public int minFallingPathSum(int[][] matrix) {
         return minPathIterative(matrix);
+
+        // int min = (int) 1e9;
+        // int row = matrix.length-1;
+        // int dp[][] = new int[row+1][matrix[0].length];
+        // for (int i = 0; i < dp.length; i++) {
+        //     Arrays.fill(dp[i], -1); 
+        // }
+
+        // for(int itr1=0; itr1<matrix[0].length;itr1++){
+        //     min = Math.min(min,minPath(row,itr1,matrix,dp));
+        // }
+        // return min;
     }
 
     private int minPathIterative(int[][] mat){
@@ -31,21 +43,21 @@ class Solution {
         return min;   
     }
     
-    private int maxPath(int row, int col, int[][] matrix,int dp[][]){
+    private int minPath(int row, int col, int[][] matrix,int dp[][]){
         if(col < 0 || col>= matrix[0].length){
-            return (int) -1e9;
+            return (int) 1e9;
         }
         if(row == 0){
             return matrix[row][col];
         }
-        if(dp[row][col]!=0){
+        if(dp[row][col]!=-1){
             return dp[row][col];
         }
         
-        int left = matrix[row][col] + maxPath(row-1,col-1,matrix,dp);
-        int up = matrix[row][col] + maxPath(row-1,col,matrix,dp);
-        int right = matrix[row][col] + maxPath(row-1,col+1,matrix,dp);
-        dp[row][col] = Math.max(Math.max(left,up),right);
+        int left = matrix[row][col] + minPath(row-1,col-1,matrix,dp);
+        int up = matrix[row][col] + minPath(row-1,col,matrix,dp);
+        int right = matrix[row][col] + minPath(row-1,col+1,matrix,dp);
+        dp[row][col] = Math.min(Math.min(left,up),right);
         return dp[row][col];
     }
 }
