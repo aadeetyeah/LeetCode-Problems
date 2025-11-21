6ce94b5b6484894271724a852ba3c2b776048070
Aditya A <39512101+aadeetyeah@users.noreply.github.com>
2025-11-21 20:02:01 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Partitions with Given Difference/partitions-with-given-difference.java
+++ Difficulty: Medium/Partitions with Given Difference/partitions-with-given-difference.java
@@ -10,7 +10,34 @@ class Solution {
         if((total-diff)%2==1 || total-diff<0){
             return 0;
         }
-        return count(arr.length-1,(total-diff)/2,arr,memo);
+        // return count(arr.length-1,(total-diff)/2,arr,memo);
+        return countIterative(arr,(total-diff)/2);
+    }
+    
+    private int countIterative(int nums[],int target){
+        int dp[][] = new int[nums.length][target+1];
+        
+        if(nums[0]==0){
+            dp[0][0] = 2;
+        }else{
+            dp[0][0] = 1;
+        }
+        
+        if(nums[0]!=0 && nums[0] <= target){
+            dp[0][nums[0]] = 1;
+        }
+        for(int itr1 = 1;itr1<nums.length;itr1++){
+            for(int itr2=0;itr2<target+1;itr2++){
+                int dontpick = dp[itr1-1][itr2];
+        
+                int pickme = 0;
+                if(nums[itr1] <= itr2){
+                    pickme = dp[itr1-1][itr2-nums[itr1]];
+                }
+                dp[itr1][itr2] =dontpick + pickme;
+            }
+        }
+        return dp[nums.length-1][target];
     }
     
     private int count(int index, int target, int[] nums,
