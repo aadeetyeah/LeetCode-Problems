8b9de0a92f3d7bbd4353945507c102a95f2ce834
Aditya A <39512101+aadeetyeah@users.noreply.github.com>
2025-12-25 19:28:44 +0530

    Time: 13 ms (45.86%) | Memory: 57.2 MB (19.53%) - LeetSync

--- 518-coin-change-ii/coin-change-ii.java
+++ 518-coin-change-ii/coin-change-ii.java
@@ -1,10 +1,11 @@
 class Solution {
     public int change(int amount, int[] coins) {
-        int dp[][] = new int[coins.length][amount+1];
-        for(int d[] : dp){
-            Arrays.fill(d,-1);
-        }
-        return f(coins.length-1,amount,coins,dp);
+        // int dp[][] = new int[coins.length][amount+1];
+        // for(int d[] : dp){
+        //     Arrays.fill(d,-1);
+        // }
+        // return f(coins.length-1,amount,coins,dp);
+        return f(amount,coins);
     }
 
     private int f(int index, int amount, int[] coins, int dp[][]){
@@ -26,5 +27,27 @@ class Solution {
         }
         dp[index][amount] = take+notTake;
         return dp[index][amount];
+    }
+
+    private int f(int amount, int[] coins){
+        int dp[][] = new int[coins.length][amount+1];
+        for(int d[] : dp){
+            Arrays.fill(d,-1);
+        }
+        for(int target = 0;target<=amount;target++){
+            dp[0][target] = (target%coins[0] == 0)?1:0 ;
+        }
+
+        for(int index=1;index<coins.length;index++){
+            for(int itr2=0;itr2<=amount;itr2++){
+                int notTake = dp[index-1][itr2];
+                int take = 0;
+                if(coins[index]<=itr2){
+                    take = dp[index][itr2-coins[index]];
+                }
+                dp[index][itr2] = take+notTake;
+            }
+        }
+        return dp[coins.length-1][amount];
     }
 }
