a3f4ce305b4a906f192f2a486e2cee95b6d24c0a
Aditya A <39512101+aadeetyeah@users.noreply.github.com>
2026-01-25 16:08:56 +0530

    Time: 45 ms (54.11%), Space: 63.9 MB (43.96%) - LeetHub

---
+++ 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.java
@@ -0,0 +1,56 @@
+class Solution {
+    public int longestPalindromeSubseq(String s) {
+        String sr = new StringBuilder(s).reverse().toString();
+         int dp[][] = new int[s.length()][sr.length()];
+        for(int d[]: dp){
+            Arrays.fill(d,-1);
+        }
+        return f(s,sr,s.length()-1,sr.length()-1,dp);
+        // return fIter(s,sr);
+    }
+
+    private int fIter(String s1,String s2){
+    int dp[][] = new int[s1.length()+1][s2.length()+1];
+    for(int d[]: dp){
+        Arrays.fill(d,-1);
+    }
+        for(int itr1 = 0;itr1<=s1.length();itr1++){
+            dp[itr1][0] = 0;
+        }
+        for(int itr2 = 0;itr2<=s2.length();itr2++){
+            dp[0][itr2] = 0;
+        }
+
+        for(int itr1 = 1;itr1<=s1.length();itr1++){
+            for(int itr2 = 1;itr2<=s2.length();itr2++){
+                if(s1.charAt(itr1-1) == s2.charAt(itr2-1)){
+                    dp[itr1][itr2] = 1 + dp[itr1-1][itr2-1]; 
+                }else{
+                    dp[itr1][itr2] = Math.max(dp[itr1-1][itr2],dp[itr1][itr2-1]);
+                }
+            }
+        }
+        for(int dp1[] : dp){
+            for(int d: dp1){
+            System.out.print(d+" ");
+            }
+            System.out.println();
+        }
+        return dp[s1.length()][s2.length()];
+    }
+
+    private int f(String s1, String s2, int i1, int i2,int dp[][]){
+        if(i1<0 || i2<0){
+            return 0;
+        }
+        if(dp[i1][i2] != -1){
+            return dp[i1][i2];
+        }
+        if(s1.charAt(i1) == s2.charAt(i2)){
+            dp[i1][i2] = 1 + f(s1,s2,i1-1,i2-1,dp);
+        }else{
+            dp[i1][i2] = Math.max(f(s1,s2,i1-1,i2,dp),f(s1,s2,i1,i2-1,dp));
+        }
+        return dp[i1][i2];
+    }
+}
