d1bac60252a72e4fed73933fd2d4d37b31bfb34a
Aditya A <39512101+aadeetyeah@users.noreply.github.com>
2026-02-03 23:53:43 +0530

    Time: 22 ms (26.37%) | Memory: 63.4 MB (7.57%) - LeetSync

--- 115-distinct-subsequences/distinct-subsequences.java
+++ 115-distinct-subsequences/distinct-subsequences.java
@@ -6,7 +6,29 @@ class Solution {
         for(int d[] : dp){
             Arrays.fill(d,-1);
         }
-        return f(s,t,l1-1,l2-1,dp);
+        // return f(s,t,l1-1,l2-1,dp);
+        return fIterative(s,t);
+    }
+
+    private int fIterative(String s1, String s2){
+        int l1 = s1.length();
+        int l2 = s2.length();
+        int dp[][] = new int[l1+1][l2+1];
+
+        for(int itr1 = 0;itr1<l1+1;itr1++){
+            dp[itr1][0] = 1;
+        }
+        
+        for(int i1 =1;i1<l1+1;i1++){
+            for(int i2 =1;i2<l2+1;i2++){
+                if(s1.charAt(i1-1) == s2.charAt(i2-1)){
+                    dp[i1][i2] = dp[i1-1][i2-1]+ dp[i1-1][i2];
+                }else{
+                    dp[i1][i2] = dp[i1-1][i2];
+                }
+            }
+        }
+        return dp[l1][l2];
     }
 
     private int f(String s1, String s2, int i1, int i2,int dp[][]){
