175578507587aa92b8f21c58fb6bface7da3283c
Aditya A <39512101+aadeetyeah@users.noreply.github.com>
2026-02-10 00:27:51 +0530

    Time: 6 ms (10.43%) | Memory: 46.9 MB (90.15%) - LeetSync

--- 72-edit-distance/edit-distance.java
+++ 72-edit-distance/edit-distance.java
@@ -6,11 +6,37 @@ class Solution {
         for(int d[] : dp){
             Arrays.fill(d,-1);
         }
-        return f(word1,word2,l1-1,l2-1,dp);
+        // return f(word1,word2,l1-1,l2-1,dp);
+        return fIterative(word1,word2);
+    }
+
+    private int fIterative(String word1, String word2){
+        int l1 = word1.length();
+        int l2 = word2.length();
+        int dp[][] = new int[l1+1][l2+1];
+        for(int d[] : dp){
+            Arrays.fill(d,-1);
+        }
+        for(int itr1=0;itr1<=l1;itr1++){
+            dp[itr1][0] = itr1;
+        }
+        for(int itr2=0;itr2<=l2;itr2++){
+            dp[0][itr2] = itr2;
+        }
+        for(int i1=1; i1<=l1; i1++){
+            for(int i2=1; i2<=l2; i2++){
+                if(word1.charAt(i1-1) == word2.charAt(i2-1)){
+                    dp[i1][i2] = dp[i1-1][i2-1];
+                    }else{
+                        dp[i1][i2] = 1 + Math.min(dp[i1][i2-1], Math.min(dp[i1-1][i2],dp[i1-1][i2-1]));
+                        }
+                        }
+        }
+        return dp[l1][l2];
     }
 
     private int f(String word1, String word2, int i1, int i2,int dp[][]){
-        if(i1<0){
+        if(i1<0){ 
             return i2+1;
         }
         if(i2<0){
