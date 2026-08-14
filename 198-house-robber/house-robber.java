90d9099aef404f5aff172713c3b7156274881fa8
Aditya A <39512101+aadeetyeah@users.noreply.github.com>
2026-08-14 16:55:25 +0530

    Time: 0 ms (100.00%) | Memory: 42.7 MB (75.75%) - LeetSync

--- 198-house-robber/house-robber.java
+++ 198-house-robber/house-robber.java
@@ -2,7 +2,23 @@ class Solution {
     public int rob(int[] nums) {
         int dp[] = new int[nums.length];
         Arrays.fill(dp,-1);
-        return rob(nums,nums.length-1,dp);
+        // return rob(nums,nums.length-1,dp);
+        return robIter(nums);
+    }
+
+    private int robIter(int nums[]){
+        int dp[] = new int[nums.length];
+        dp[0] = nums[0];
+        for(int itr1=1; itr1<nums.length; itr1++){
+            int pick = nums[itr1];
+            if(itr1>1){
+                pick +=  dp[itr1-2];
+            }
+            int notpick = dp[itr1-1];
+            dp[itr1] = Math.max(pick, notpick);
+        }
+
+        return dp[nums.length-1];
     }
 
     private int rob(int[] nums, int index,int dp[]){
