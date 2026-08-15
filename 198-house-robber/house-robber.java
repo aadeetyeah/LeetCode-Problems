b9e8a7b25a5ce002b6a369679fafbcf597f381e2
Aditya A <39512101+aadeetyeah@users.noreply.github.com>
2026-08-15 20:19:06 +0530

    Time: 0 ms (100.00%) | Memory: 42.8 MB (57.43%) - LeetSync

--- 198-house-robber/house-robber.java
+++ 198-house-robber/house-robber.java
@@ -2,40 +2,19 @@ class Solution {
     public int rob(int[] nums) {
         int dp[] = new int[nums.length];
         Arrays.fill(dp,-1);
-        // return rob(nums,nums.length-1,dp);
-        return robIter(nums);
+        return robF(nums,nums.length-1,dp);
     }
 
-    private int robIter(int nums[]){
-        int dp[] = new int[nums.length];
-        dp[0] = nums[0];
-        for(int itr1=1; itr1<nums.length; itr1++){
-            int pick = nums[itr1];
-            if(itr1>1){
-                pick +=  dp[itr1-2];
-            }
-            int notpick = dp[itr1-1];
-            dp[itr1] = Math.max(pick, notpick);
-        }
-
-        return dp[nums.length-1];
-    }
-
-    private int rob(int[] nums, int index,int dp[]){
-
-        if(index == 0){
-            return nums[0];
-        }
+    private int robF(int nums[], int index,int dp[]){
         if(index < 0){
             return 0;
-        }        
+        }
         if(dp[index]!=-1){
             return dp[index];
         }
-        int pick = nums[index] + rob(nums,index-2,dp);
-        int notpick = rob(nums,index-1,dp);
-
-        dp[index] = Math.max(pick,notpick);
+        int rob = nums[index] + robF(nums,index-2,dp);
+        int drob = robF(nums,index-1,dp);
+        dp[index] = Math.max(rob,drob);
         return dp[index];
     }
 }
