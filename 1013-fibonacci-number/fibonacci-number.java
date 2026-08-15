6981af05079f6a384c5178cab54b0f931eddeea8
Aditya A <39512101+aadeetyeah@users.noreply.github.com>
2026-08-15 18:52:49 +0530

    Time: 10 ms (26.38%) | Memory: 42 MB (70.90%) - LeetSync

---
+++ 1013-fibonacci-number/fibonacci-number.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int fib(int n) {
+        return fibonacci(n);
+    }
+
+    private int fibonacci(int n){
+        if(n== 0 || n == 1){
+            return n;
+        }
+
+        return fibonacci(n-1) + fibonacci(n-2);
+    }
+}
