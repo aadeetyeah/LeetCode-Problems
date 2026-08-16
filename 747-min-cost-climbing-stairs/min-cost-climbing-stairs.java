ddf2ac6976dad4c8174f135ffb975f09435b395b
Aditya A <39512101+aadeetyeah@users.noreply.github.com>
2026-08-16 21:57:09 +0530

    Time: 38 ms (0.82%) | Memory: 46.2 MB (9.91%) - LeetSync

--- 747-min-cost-climbing-stairs/min-cost-climbing-stairs.java
+++ 747-min-cost-climbing-stairs/min-cost-climbing-stairs.java
@@ -2,37 +2,29 @@ class Solution {
     public int minCostClimbingStairs(int[] cost) {
         int dp[] = new int[cost.length];
         Arrays.fill(dp,-1);
-        // return Math.min(minCost(cost,cost.length-1,dp),minCost(cost,cost.length-2,dp)); 
-        return minCostIterative(cost);
+        dp[cost.length-1] = minCost(cost,cost.length-1,dp);
+        for(int itr : dp){
+            System.out.println(itr);
+        }
+        return Math.min(dp[cost.length-1],dp[cost.length-2]);
     }
 
     private int minCost(int[] cost, int index,int dp[]){
-        if(index == 0 || index == 1 ){
+        if(index == 0){
+            dp[index] = cost[index];
             return cost[index];
         }
-        if(index<0){
+        if(index < 0){
             return 0;
         }
         if(dp[index]!=-1){
             return dp[index];
         }
 
-        int oneStep = minCost(cost,index-1,dp);
-        int twoStep = minCost(cost,index-2,dp);
-
-        dp[index] = cost[index] + Math.min(oneStep,twoStep);
+        int os = minCost(cost, index-1,dp);
+        int ts = minCost(cost, index-2,dp);
+        int res = cost[index] + Math.min(os,ts);
+        dp[index] = res;
         return dp[index];
-    }
-
-    private int minCostIterative(int cost[]){
-        int dp[] = new int[cost.length];
-
-        for(int itr1 = 0;itr1<cost.length;itr1++){
-            if(itr1<2)
-                dp[itr1] = cost[itr1];
-            else
-                dp[itr1] = cost[itr1] + Math.min(dp[itr1-1],dp[itr1-2]);
-        }
-        return Math.min(dp[cost.length-1],dp[cost.length-2]);
     }
 }
